8b9556e56b92122b003622dee7288c1e57428c0d
Dvir Yitzchaki <dyitzchaki@oath.com>
2018-09-16 23:43:21 +0300

    added option to delete duplicates

--- gmusicapi_scripts/gmdelete.py
+++ gmusicapi_scripts/gmdelete.py
@@ -27,6 +27,8 @@ Options:
                                         This option can be set multiple times.
   -a, --all-includes                    Songs must match all include filter criteria to be included.
   -A, --all-excludes                    Songs must match all exclude filter criteria to be excluded.
+	-D METHOD, --duplicates METHOD        Delete duplicate songs.
+																				METHOD can be OLD/NEW for deleting all but the newest/oldest song respectively.
   -y, --yes                             Delete songs without asking for confirmation.
 
 Patterns can be any valid Python regex patterns.
@@ -34,6 +36,8 @@ Patterns can be any valid Python regex patterns.
 
 import logging
 import sys
+from datetime import datetime
+from itertools import tee
 
 from docopt import docopt
 
@@ -49,6 +53,10 @@ logger.addHandler(sh)
 
 def main():
 	cli = dict((key.lstrip("-<").rstrip(">"), value) for key, value in docopt(__doc__).items())
+
+	if cli['duplicates'] and cli['duplicates'] not in ['OLD', 'NEW']:
+		print(__doc__)
+		sys.exit()
 
 	if cli['quiet']:
 		logger.setLevel(QUIET)
@@ -68,6 +76,25 @@ def main():
 		include_filters=include_filters, exclude_filters=exclude_filters,
 		all_includes=cli['all-includes'], all_excludes=cli['all-excludes']
 	)
+
+	if cli['duplicates']:
+		def duplicate_gen(songs):
+			cmp_fields = ['albumArtist', 'album', 'title', 'artist', 'trackNumber']
+
+			def cmp_values(song):
+				return tuple(map(song.get, cmp_fields))
+			songs = sorted(
+				filter(
+					lambda song: all(key in song for key in (cmp_fields + ['lastModifiedTimestamp'])),
+					songs
+				),
+				key=lambda song: cmp_values(song) + (datetime.fromtimestamp(int(song['lastModifiedTimestamp']) / 10**6),),
+				reverse=cli['duplicates'] == 'NEW'
+			)
+			for i, j in zip(songs, songs[1:]):
+				if cmp_values(i) == cmp_values(j):
+					yield j
+		songs_to_delete = list(duplicate_gen(songs_to_delete))
 
 	if cli['dry-run']:
 		logger.info("Found {0} songs to delete".format(len(songs_to_delete)))
